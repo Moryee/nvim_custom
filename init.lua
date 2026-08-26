2f28c50d0dd4840e6eb65c73b78dbcb2c1eafd81
Oleksandr Fedorutsa <oleksandr.fedorutsa@propylon.com>
2026-08-26 14:52:20 +0100

    feat: add prettier formatter for json

--- init.lua
+++ init.lua
@@ -842,6 +842,7 @@ do
       --
       -- You can use 'stop_after_first' to run the first available formatter from the list
       -- javascript = { "prettierd", "prettier", stop_after_first = true },
+      json = { 'prettier' },
     },
   }
 
